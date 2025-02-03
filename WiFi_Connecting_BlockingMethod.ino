24412d99155a1f2ebf10a01912c1dc0ca98b8b69
Neethu Jaisan <134039864+Neethu-Jaisan@users.noreply.github.com>
2025-02-03 16:02:30 +0530

    Create WiFi_Connecting_BlockingMethod.ino

---
+++ WiFi_Connecting_BlockingMethod.ino
@@ -0,0 +1,26 @@
+//Write "monitor_speed= 921600" in paltformio.ini
+
+#include <Arduino.h>
+#include <WiFiMulti.h>
+
+#define WIFI_SSID "ICFOSS"
+#define WIFI_PASSWORD "n0tava!lableG32!"
+
+WiFiMulti wifiMulti;
+
+void setup() {
+  Serial.begin(921600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+  while(wifiMulti.run()!= WL_CONNECTED){
+    delay(100);
+  }
+
+  Serial.println("Connected");
+}
+  void loop()
+  {
+    digitalWrite(LED_BUILTIN, WiFi.status() == WL_CONNECTED);
+  }
